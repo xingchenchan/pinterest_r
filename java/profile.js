97201930bb15f3fceae43a157483505866df094a
xingchen33 <tippi2935480894@gmail.com>
2024-11-20 14:15:15 -0800

    hamu

--- java/profile.js
+++ java/profile.js
@@ -1,53 +1,52 @@
-var gray200="#e9e9e9"
-var gray100 ="#f1f1f1"
+// var gray200="#e9e9e9"
+// var gray100 ="#f1f1f1"
 
-function buttonclick(){
-    console.log("button was clicked")
-    const button = document.querySelector('button')
-    button.addEventListener('mouseover', () => {
-        button.style.backgroundColor = gray200
-    })
+// function inputClick(){
+//     console.log("input was clicked")
+//     const sv = document.querySelector('svg')
+//     const aw = document.querySelector('a')
 
-    button.addEventListener('mouseleave', () => {
-        button.style.backgroundColor = "white"
-    })
-    console.log("finished")
+//     div.addEventListener('click', () => {
+//         sv.style.oapcity = 0
+//         aw.style.opacity = 0
+//     })
 
-    return 
+//     div.addEventListener('mouseleave', () => {
+//         div.style.backgroundColor = gray100
+//     })
+//     console.log("finished")
 
-    // cannot be applied to other functions
-}
+//     const an = document.createElement("a")
+//     an.textContent = "Try a title or description"
+//     ul.appendChild(an)
 
-function divclick(){
-    console.log("div was clicked")
-    const div = document.querySelector('div')
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = "red"
-    })
+//     return 
 
-    div.addEventListener('mouseleave', () => {
-        div.style.backgroundColor = "white"
-    })
-    console.log("finished")
+// }
 
-    return 
+// function inputClick(){
+//     console.log("input was clicked")
+//     const sv = document.querySelector('svg')
+//     const aw = document.querySelector('a')
 
-}
+//     div.addEventListener('click', () => {
+//         sv.style.oapcity = 0
+//         aw.style.opacity = 0
+//     })
 
-function inputclick(){
-    console.log("input was clicked")
-    const div = document.querySelector('input')
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = gray200
-    })
+//     div.addEventListener('mouseleave', () => {
+//         div.style.backgroundColor = gray100
+//     })
+//     console.log("finished")
 
-    div.addEventListener('mouseleave', () => {
-        div.style.backgroundColor = gray100
-    })
-    console.log("finished")
+//     const an = document.createElement("a")
+//     an.textContent = "Try a title or description"
+//     ul.appendChild(an)
 
-    return 
-}
+//     return 
+
+// }
+
 
 
 
