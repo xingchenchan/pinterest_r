4a38df1fcdf17c2bad875d6a35e2606a6c3ca08b
xingchen33 <tippi2935480894@gmail.com>
2024-11-22 11:02:55 -0800

    h

--- my-app/static/java/profile.js
+++ my-app/static/java/profile.js
@@ -1,23 +1,30 @@
-function testadd(){
-    console.log("button activated")
+// function newsaved(){
+//     console.log("tunning second function")
 
-    const img = document.createElement("img")
+//     const newlist = document.createElement("ul")
 
-    img.src = "c70b5793c38852288ff2ded36ef52fc8.jpg"
-
-    const newlist = document.createElement("ul")
-
-    const newli = document.createElement("li")
+//     const newli = document.createElement("li")
     
+//     const container = document.getElementById("mypins")
+
+//     container.appendChild(newlist)
+
+//     newlist.appendChild(newli)
+
+//     newli.appendChild(img)
+
+//     console.log("done")
+
+// }
+
+function newsaved(){
+    console.log("tunning second function")
+
     const container = document.getElementById("mypins")
 
-    container.appendChild(newlist)
+    console.log("finish second function")
 
-    newlist.appendChild(newli)
-
-    newli.appendChild(img)
-
-    console.log("done")
+    return container
 
 }
 
